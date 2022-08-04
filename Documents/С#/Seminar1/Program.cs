8fa09d4b34af6005696bfc4f5ff401399a042165
Veronika <valeria14081981@yandex.com>
2022-08-04 21:33:45 +0300

    Zadacha

--- Documents/С#/Seminar1/Program.cs
+++ Documents/С#/Seminar1/Program.cs
@@ -1,16 +1,18 @@
-﻿/*
-Напишите программу, которая на вход принимает число и выдает его квадрат
+﻿Console.Write("Введите число №1: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Например:
-4- > 16
--3 -> 9
--7 -> 49
-*/
+Console.Write("Введите число №2: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число:");
+if (firstNumber == secondNumber * secondNumber) //f = 25, s = 5
+{
+    Console.WriteLine(firstNumber + " является квадратом от " + secondNumber);
+    // Конкатенация - процесс сложения строк: "1" + "1" = "11"
+}
+// "!=" -  проверка на неравенство 5 != 5 - False
+// "==" - равенство. 5 == 5 - True
+else // if (firstNumber != secondNumber * secondNumber)
+{
+    Console.WriteLine(firstNumber + " НЕ является квадратом от " + secondNumber);
+}
 
-int number = Convert.ToInt32(Console.ReadLine());
-
-int square = number * number;
-
-Console.WriteLine(square);
