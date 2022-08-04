0bb3aaf86671331b72b73dc722ce9c6ef27b8c55
Veronika <valeria14081981@yandex.com>
2022-08-04 21:14:46 +0300

    *

---
+++ Documents/С#/Seminar1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Например:
+4- > 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.Write("Введите число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine(square);
---
+++ OneDrive/Рабочий стол/Zadanie/Examples/Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите ваше имя");
+String username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
+
